c0f14c740f6c4711a926d48f4238abf40fc4fb0d
Shreejanshres <shreejan.shrestha2003@gmail.com>
2024-09-12 16:40:07 +0545

    completed all final ver

--- lib/cubit/weather_cubit.dart
+++ lib/cubit/weather_cubit.dart
@@ -4,12 +4,11 @@ import 'package:weatherapp/repo/weather_repo.dart';
 
 class WeatherCubit extends Cubit<WeatherState> {
   final WeatherRepository _weatherRepository = WeatherRepository();
-  final String city;
+  // final String city;
 
-  WeatherCubit(this.city) : super(WeatherInitial());
+  WeatherCubit() : super(WeatherInitial());
 
   void getweather(String city) async {
-    print(city);
     try {
       emit(WeatherLoading());
       final weather = await _weatherRepository.fetchWeather(city);
--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => WeatherCubit('kathmandu'),
+      create: (context) => WeatherCubit(),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Weather App',
